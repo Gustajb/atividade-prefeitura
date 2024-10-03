ac842bd0ad35bdcef840c253215d3842af140428
Gustajb <gustavobatistagb09@gmail.com>
2024-10-03 16:23:16 +0000

    salvar

--- gustavo.py
+++ gustavo.py
@@ -1,26 +1,36 @@
 import os
+from dataclasses import dataclass
+
 os.system("cls || clear")
 
-# Apresentando MENU.
-print("=== EXIBINDO MENU ===")
-print("""
-      Código | Descrição
-      1      | Adicionar Família
-      2      | Exibir resultados 
-      3      | Sair
-      """ )
+@dataclass
+class Pesquisa:
+    nome: str
+    numero_de_filhos: int
+    salario: float
 
-for i in range():
-    opcao = int(input("Digite o código para escolher uma opção: "))
+def media(a,b):
+    mediafim = a/b
+    return mediafim
+def maior_menor(a):
+    menor = min(lista_de_salarios)
+    maior = max(lista_de_salarios)
+    return maior, menor
 
-# 
-match(opcao):
-    case 1:
-        
-    case 2:
-        
-       
-    case 3:
-     
-        
+lista_de_familias = []
+lista_de_salarios = []
 
+numero_familias = 0
+contador_salario = 0
+contador_filhos = 0
+
+while True:
+    print("""
+            === Menu ===
+    |Código |   Descrição       |
+    |   1   | Adicionar família |
+    |   2   | Exibir resultados |
+    |   3   | Sair              |
+          """)
+    menu = int(input("Digite uma opção: "))
+    os.system("cls || clear")
